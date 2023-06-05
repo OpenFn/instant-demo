31458e56b6133e6bb09be5e0ef54e40df3a0b705
mtuchi <mtuchidev@gmail.com>
2023-06-05 16:01:45 +0300

    feat: create bundle to fhir

--- lightning/createPatientBundleFHIR.js
+++ lightning/createPatientBundleFHIR.js
@@ -1,1 +1,26 @@
-create('Bundle', state => state.patientResource);
+// create('Bundle', state => state.patientResource);
+// fn(state => {
+//   console.log(JSON.stringify(state.data, null, 2));
+//   return state;
+// });
+
+create('Bundle', state => ({...state.data, type: 'collection' }));
+// create('', state => ({...state.data, type: 'transaction' }));
+
+// create('Bundle', state => {
+//   const { data } = state;
+//   delete data.link;
+//   delete data.total;
+//   const { resourceType, entry } = data;
+
+//   const payload = {
+//     resourceType,
+//     // entry: entry.map(i => ({ fullUrl: i.fullUrl, resource: i.resource })),
+//     entry,
+//     type: 'collection',
+//   };
+
+//   // console.log(JSON.stringify(payload, null, 2));
+
+//   return payload;
+// });
--- lightning/getPatientBundleFHIR.js
+++ lightning/getPatientBundleFHIR.js
@@ -1,8 +1,9 @@
 // Get patient resource bunlde
-get('Bundle/c393585c-1f5d-4eab-b19e-2c51bdce0d11', {}, state => {
-  const patientResource = state.data;
-  return { ...state, data: {}, response: {}, references: [], patientResource };
-});
+// get('Bundle/c393585c-1f5d-4eab-b19e-2c51bdce0d11');
 
+get('Patient');
+
+// create('Bundle', state => state.data);
 // Create new patents in HAPI FHIR
 // createTransactionBundle(state => state.patientResource);
+// create('Bundle', state => state.patientResource);
